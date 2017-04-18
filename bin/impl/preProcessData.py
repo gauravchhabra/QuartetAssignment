8d11cac96c5137437650cbc12bb6b86f1662e4ce
gachhabra <gachhabra@paypal.com>
2017-04-18 11:00:10 -0700

    Placeholder functions for preProcessingData Step

--- bin/impl/preProcessData.py
+++ bin/impl/preProcessData.py
@@ -1,4 +1,16 @@
+#Placeholders for Helper functions 'removeExtraSpaces' & 'normalizeAddress'
+def removeExtraSpaces(data):
+    return data
+
+def normalizeAddress(address):
+    normalizeAddress = address
+    return normalizedAddress
 
 #Placeholder function for enriching and normalizing data
 def getEnrichedAndNormalizedRecord(record):
+    #Iterate over all fields of the record
+    for field in record:
+        removeExtraSpaces(field)
+        if field is address:
+            normalizeAddress(field)
     return data
